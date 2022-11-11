d664a5d037aefd795575f0799ed4d4233de450d2
7twik <99895046+7twik@users.noreply.github.com>
2022-11-12 02:41:25 +0530

    Add files via upload

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,45 @@
+// import { Link } from "react-router-dom";
+import styles from "./styles.module.css";
+import React from "react";
+function Login() {
+	
+	const googleAuth = () => {
+		window.open(
+			`https://back-trackersy.onrender.com/auth/google/callback`,
+			"_self"
+		);
+	};
+	return (
+		
+		<div className={styles.bgm}>
+		<div className={styles.container}>
+			
+			<div className={styles.form_container}>
+			
+				<div className={styles.left}>
+				{/* <h3>{user.q}</h3> */}
+					{/* <img className={styles.img} src="https://source.unsplash.com/1600x900/?money" alt="login" />  */}
+					<img className={styles.img} src="./images/ppnobg2.png" alt="login" /> 
+				</div>
+				<div className={styles.right}>
+				
+					<h2 className={styles.from_heading}>Members Log in</h2>
+					{/* <input type="text" className={styles.input} placeholder="Email" />
+					<input type="text" className={styles.input} placeholder="Password" /> */}
+					{/* <button className={styles.btn}>Log In</button> */}
+					<button className={styles.google_btn} onClick={googleAuth}>
+						<img src="./images/google.png" alt="google icon" />
+						<span>Sign in with Google</span>
+					</button>
+					{/* <p className={styles.text}>
+						New Here ? <Link to="/signup">Sign Up</Link>
+					</p> */}
+				</div>
+			</div>
+		</div>
+		</div>
+		
+	);
+}
+
+export default Login;
